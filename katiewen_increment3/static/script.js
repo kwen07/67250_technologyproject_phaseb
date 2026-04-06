ee4e93af6284c1140af316dae244ca7abef292c5
kwen07 <y.katiewen@gmail.com>
2026-04-06 01:29:53 -0400

    Add files via upload

---
+++ katiewen_increment3/static/script.js
@@ -0,0 +1,69 @@
+var x = 5;
+var y = 7;
+var z = x + y;
+console.log(z);
+
+var A = "Hello ";
+var B = "world!";
+var C = A + B;
+console.log(C);
+
+function sumnPrint(x1, x2) {
+  var result = x1 + x2;
+  console.log(result);
+}
+
+sumnPrint(x, y);
+sumnPrint(A, B);
+
+if (C.length > z) {
+    if (C.length < z) {
+        console.log(C);
+    }
+} else if (C.length < z) {
+    console.log(z);
+} else {
+    console.log("good job!");
+}
+
+// var L1 = ["Watermelon", "Pineapple", "Pear", "Banana"];
+// var L2 = ["Apple", "Banana", "Kiwi", "Orange"];
+
+// function findTheBanana(arr) {
+//     arr.forEach(function(item) {
+//         if (item === "Banana") {
+//             alert("Banana found!");
+//         }
+//     });
+// }
+
+// findTheBanana(L1);
+// findTheBanana(L2);
+
+var now = new Date();
+var hour = now.getHours();
+
+function greeting(h) {
+    var greetingElement = document.getElementById("greeting");
+    if (greetingElement) {
+        if (h < 5 || h >= 20) {
+            greetingElement.innerHTML = "Good night.";
+        } else if (h < 12) {
+            greetingElement.innerHTML = "Good morning.";
+        } else if (h < 18) {
+            greetingElement.innerHTML = "Good afternoon.";
+        } else {
+            greetingElement.innerHTML = "Good evening.";
+        }
+    }
+}
+
+greeting(hour);
+
+function addYear() {
+    var year = new Date().getFullYear();
+    var yearElement = document.getElementById("copyYear");
+    if (yearElement) {
+        yearElement.innerHTML = year;
+    }
+}
